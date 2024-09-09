64d0e3a23a83f6e3abf4b1328a5a506b05eb4dbf
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-09 15:09:15 +0800

    style: modify the open-popup

--- javascript/script.js
+++ javascript/script.js
@@ -2,5 +2,10 @@ const submitBtn = document.querySelector("#submit-btn");
 const okBtn = document.querySelector("#ok-btn");
 const popup = docoument.querySelector("#popup");
 
+function openPopup() {
+
+}
+
+
 submitBtn.addEventListener("click", openPopup);
 okBtn.addEventListener("click", closePopup);
