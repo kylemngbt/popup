1bd1de9a3600a9c26130c7ba9f7479f403758d03
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-09 15:12:28 +0800

    feat: define openPopup and closePopup functions

--- javascript/script.js
+++ javascript/script.js
@@ -2,5 +2,14 @@ const submitBtn = document.querySelector("#submit-btn");
 const okBtn = document.querySelector("#ok-btn");
 const popup = document.querySelector("#popup");
 
+function openPopup() {
+  popup.classList.add("open-popup");
+}
+
+function closePopup() {
+  popup.classList.remove("open-popup");
+}
+
+
 submitBtn.addEventListener("click", openPopup);
 okBtn.addEventListener("click", closePopup);
