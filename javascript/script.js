6b970eefd53c68691666e7f3939037c98e279bb8
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-09 15:07:54 +0800

    build: create a reference of the popup

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 const submitBtn = document.querySelector("#submit-btn");
 const okBtn = document.querySelector("#ok-btn");
+const popup = docoument.querySelector("#popup");
 
 submitBtn.addEventListener("click", openPopup);
 okBtn.addEventListener("click", closePopup);
